db751f365962bb63ad5b891b2c945f9c20d0bd0c
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-10-04 04:20:11 -0400

    Bibliogram & nitter working

--- src/privateRedirect.js
+++ src/privateRedirect.js
@@ -2,6 +2,25 @@
  * Reconstruct social links to privacy-friendly front-ends
  */
 
-function privateRedirect() {
-    //TODO
+function privateRedirect(type, username) {
+    var element = document.getElementById(type + "-output");
+    element.innerHTML = "";
+    if (!isEmpty(username)) {
+        var linkFirstPart = "<a target='_blank' href='";
+        var linkSecondPart = "'>"
+        var linkThirdPart = "</a>"
+        if (type == "bibliogram") {
+            var bibliogram = "https://bibliogram.art/u/" + username;
+            element.innerHTML = linkFirstPart + bibliogram + linkSecondPart + bibliogram + linkThirdPart;
+        } else if (type == "nitter") {
+            var nitter = "https://nitter.net/" + username;
+            element.innerHTML = linkFirstPart + nitter + linkSecondPart + nitter + linkThirdPart;
+        } else if (type == "invidious") {
+            // TODO
+        }
+    }
+}
+
+function isEmpty(str) {
+    return str === null || str.match(/^\s*$/) !== null;
 }
