9d63cc7a41ad8afac2c6e846115182bdb76c8e62
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-10-04 14:05:56 -0400

    Concise implementation

--- src/privateRedirect.js
+++ src/privateRedirect.js
@@ -2,25 +2,28 @@
  * Reconstruct social links to privacy-friendly front-ends
  */
 
-function privateRedirect(type, username) {
-    var element = document.getElementById(type + "-output");
-    element.innerHTML = "";
+function privateRedirect(element) {
+    var id = element.id;
+    var outputElement = document.getElementById(id + "-output");
+    var username = element.value;
+    outputElement.innerHTML = "";
     if (!isEmpty(username)) {
-        var linkFirstPart = "<a target='_blank' href='";
-        var linkSecondPart = "'>"
-        var linkThirdPart = "</a>"
-        if (type == "bibliogram") {
-            var bibliogram = "https://bibliogram.art/u/" + username;
-            element.innerHTML = linkFirstPart + bibliogram + linkSecondPart + bibliogram + linkThirdPart;
-        } else if (type == "nitter") {
-            var nitter = "https://nitter.net/" + username;
-            element.innerHTML = linkFirstPart + nitter + linkSecondPart + nitter + linkThirdPart;
-        } else if (type == "invidious") {
+        if (id == "bibliogram") {
+            outputElement.innerHTML = createLink("https://bibliogram.art/u/" + username);
+        } else if (id == "nitter") {
+            outputElement.innerHTML = createLink("https://nitter.net/" + username);
+        } else if (id == "invidious") {
             // TODO
         }
+    } else {
+        element.value = "";
     }
 }
 
 function isEmpty(str) {
-    return str === null || str.match(/^\s*$/) !== null;
+    return str === null || /\s/.test(str);
+}
+
+function createLink(site) {
+    return "<a target='_blank' href='" + site + "'>" + site + "</a>";
 }
