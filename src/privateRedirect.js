2c4eecaa0c59f065f91285369a864252308721ee
Michael A. Agarenzo <magarenzo@gmail.com>
2020-10-07 14:32:42 -0400

    Add comments

--- src/privateRedirect.js
+++ src/privateRedirect.js
@@ -2,11 +2,19 @@
  * Reconstruct social links to privacy-friendly front-ends
  */
 
+// Driver function
 function privateRedirect(element) {
     var id = element.id;
     var outputElement = document.getElementById(id + "-output");
     var input = element.value;
+
     outputElement.innerHTML = "";
+
+    /*
+    * If input isNotEmpty, converts Instagram links to Bibliogram and/or converts Twitter links to nitter
+    * If bibliogram input contains key, the input is treated as a post, otherwise its treated as a username
+    * If nitter input contains key, the input is treated as a tweet, otherwise its treated as a username
+    */
     if (isNotEmpty(input)) {
         if (id == "bibliogram") {
             var bibliogram = "https://bibliogram.art/";
@@ -30,14 +38,17 @@ function privateRedirect(element) {
     }
 }
 
+// Returns True if passed-in str is not empty, null, or contains any spaces, tabs, etc.
 function isNotEmpty(str) {
     return !(str == "" || str === null || /\s/.test(str));
 }
 
+// Returns <a> element for passed-in site
 function createLink(site) {
     return "<a target='_blank' href='" + site + "'>" + site + "</a>";
 }
 
+// Sets value of passed-in element and inner HTML of its according output element to empty space
 function reset(element) {
     element.value = "";
     document.getElementById(element.id + "-output").innerHTML = "";
